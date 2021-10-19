676de53c7c22d73f516dd0a2737bc00879342a1f
zde82904 <85751817+zde82904@users.noreply.github.com>
2021-10-19 07:34:59 -0400

    Altered Num Variable and added a comment to code

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict";
+// Finds Message Bar
 let $in = $(`#${findChat()}`).find('> div');
+// Message you want to send
 let message = 'yes';
-let num = 50;
+// Amount of Messages you want to send
+let num = 150;
+//The Looping
 for (let i = 0; i < num; i++) {
     $in.html(message);
     $('#send-message-button').click();
@@ -17,4 +21,7 @@ function findChat() {
         });
     return name[0]; // First element always should be the right one
 }
+
+// Credits
 // Script by Roki written at 26.10.2020
+// Script Altered and Improved by Zde82904 on 19.10.2021
