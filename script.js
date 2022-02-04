2e08864ac494920654840870f07e79965935cd28
Zachary Elrod <85751817+zde82904@users.noreply.github.com>
2022-02-04 12:59:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Finds Message Bar
 let $in = $(`#${findChat()}`).find('> div');
 // Message you want to send
-let message = 'yes';
+let message = 'There I was back to back with Jesus. Bible in my right hand machine gun in the left. Ready to bless these unholy thots with the divine sauce. I ran out of ammo so I yelled across the orphanage Jesus I need a melee weapon. Never in my life have I seen a Mexican throw a sandal so fast in my life. 657 prostitutes fell that day to the holy sandal of Mexican Jesus. That day went down in history as The Great Coochie Wars';
 // Amount of Messages you want to send
 let num = 150;
 //The Looping
